586be879d6cf1e9822cb6e7326320e01a2802fc4
David Wadden <dave.wadden@gmail.com>
2017-01-18 18:17:50 -0800

    Revert SGLD to state on master.

--- edward/inferences/sgld.py
+++ edward/inferences/sgld.py
@@ -49,7 +49,6 @@ class SGLD(MonteCarlo):
     step_size : float, optional
       Constant scale factor of learning rate.
     """
-    print(step_size)
     self.step_size = step_size
     return super(SGLD, self).initialize(*args, **kwargs)
 
@@ -62,8 +61,7 @@ class SGLD(MonteCarlo):
                   for z, qz in six.iteritems(self.latent_vars)}
 
     # Simulate Langevin dynamics.
-    # learning_rate = self.step_size / tf.cast(self.t + 1, tf.float32)   # Adaptive
-    learning_rate = self.step_size * 0.01 # Not adaptive
+    learning_rate = self.step_size / tf.cast(self.t + 1, tf.float32)
     grad_log_joint = tf.gradients(self._log_joint(old_sample),
                                   list(six.itervalues(old_sample)))
     sample = {}
@@ -74,9 +72,6 @@ class SGLD(MonteCarlo):
       event_shape = qz.get_event_shape()
       normal = Normal(mu=tf.zeros(event_shape),
                       sigma=learning_rate * tf.ones(event_shape))
-      # normal = Normal(mu=tf.zeros(event_shape),
-      #                 sigma=5. * learning_rate * tf.ones(event_shape))   # crank up sigma
-
       sample[z] = old_sample[z] + 0.5 * learning_rate * grad_log_p + \
           normal.sample()
 
